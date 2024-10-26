50fc161ae2057af92e73c7c67ff5e4d654769e60
Aniketh <ambitious.abhiram001@gmail.com>
2024-10-26 08:47:59 +0530

    added upload option

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 // Nav bar open and close
 const openMenu = document.querySelector(".header-left-section"),
-  closeMenu = document.querySelector(".close-menu"),
-  sidebarNav = document.querySelector(".sidebar-nav");
+    closeMenu = document.querySelector(".close-menu"),
+    sidebarNav = document.querySelector(".sidebar-nav");
 
 openMenu.addEventListener("click", () => {
-  sidebarNav.classList.add("open-nav");
+    sidebarNav.classList.add("open-nav");
 });
 
 closeMenu.addEventListener("click", () => {
-  sidebarNav.classList.remove("open-nav");
+    sidebarNav.classList.remove("open-nav");
 });
+
+document
+    .querySelector(".connect-btn-large" || "connect-btn-small")
+    .addEventListener("click", function () {
+        document.getElementById("camera-input").click();
+    });
